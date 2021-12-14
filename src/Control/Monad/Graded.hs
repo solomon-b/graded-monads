93aab3aa6b3da1aa9d8496d3560e13567aff0050
solomon <ssbothwell@gmail.com>
2021-12-14 15:08:59 -0800

    Used Tensored from monoidal-functors

--- src/Control/Monad/Graded.hs
+++ src/Control/Monad/Graded.hs
@@ -20,74 +20,12 @@ module Control.Monad.Graded where
 import Data.Functor.Compose
 import Data.Functor.Identity
 import Data.Kind
-import Control.Category.Tensor
+import Control.Category.Tensor.Expr
 
 type (~>) f g = forall x. f x -> g x
 
 type FunctorF :: (k -> Type -> Type) -> Constraint
 type FunctorF m = (forall x. Functor (m x))
-
-type Many
-  :: (Type -> Type -> Type)
-  -> Type
-  -> [Type]
-  -> Type
-data Many t i vs
-  where
-  ANil :: i -> Many t i '[]
-  ACons :: t v (Many t i vs) -> Many t i (v ': vs)
-
-instance (AsNested t i xs r, Show r) => Show (Many t i xs)
-  where
-  show = show . fwd asNested
-
-type AsNested :: (Type -> Type -> Type) -> Type -> [Type] -> Type -> Constraint
-class Tensor t i (->) => AsNested t i xs unwrapped | t i xs -> unwrapped
-  where
-  asNested :: Tensor t i (->) => Iso (->) (Many t i xs) unwrapped
-
-instance Tensor t i (->) => AsNested t i '[] i
-  where
-  asNested :: Tensor t i (->) => Iso (->) (Many t i '[]) i
-  asNested = Iso to from
-    where
-      to :: Tensor t i (->) => Many t i '[] -> i
-      to = \case
-        ANil i -> i
-      from :: Tensor t i (->) => i -> Many t i '[]
-      from = ANil 
-  
-instance (Tensor t i (->), AsNested t i xs r) => AsNested t i (x ': xs) (x `t` r)
-  where
-  asNested :: Tensor t i (->) => Iso (->) (Many t i (x ': xs)) (x `t` r)
-  asNested = Iso to from
-    where
-      to :: (AsNested t i xs r, Tensor t i (->)) => Many t i (x ': xs) -> (x `t` r)
-      to = \case
-        ACons xxs -> grmap (fwd asNested) xxs
-      from :: (AsNested t i xs r, Tensor t i (->)) => (x `t` r) -> Many t i (x ': xs)
-      from xtr = ACons $ grmap (bwd asNested) xtr
-
-type (++) :: [k] -> [k] -> [k]
-type family xs ++ ys
-  where
-  '[] ++ xs = xs
-  (x ': xs) ++ ys = x ': (xs ++ ys)
-
-class AppendMany xs
-  where
-  appendMany
-    :: Tensor t i (->)
-    => Many t i xs `t` Many t i ys
-    -> Many t i (xs ++ ys)
-
-instance AppendMany '[]
-  where
-  appendMany = fwd lunit . glmap (\case { ANil i -> i })
-
-instance AppendMany xs => AppendMany (x ': xs)
-  where
-  appendMany = ACons . grmap appendMany . bwd assoc . glmap (\case { ACons i -> i })
 
 type GradedMonad
   :: ([Type] -> Type -> Type)
@@ -98,10 +36,10 @@ class FunctorF m => GradedMonad m i t | m -> i t where
   {-# MINIMAL greturn, (gjoin | gbind) #-}
   greturn :: Identity ~> m '[]
 
-  gjoin :: AppendMany xs => (m xs `Compose` m ys) ~> m (xs ++ ys)
+  gjoin :: AppendTensored xs => (m xs `Compose` m ys) ~> m (xs ++ ys)
   gjoin (Compose m) = m `gbind` id
 
-  gbind :: AppendMany xs => m xs a -> (a -> m ys b) -> m (xs ++ ys) b
+  gbind :: AppendTensored xs => m xs a -> (a -> m ys b) -> m (xs ++ ys) b
   gbind mxa f = gjoin $ Compose $ fmap f mxa
 
 return
@@ -112,7 +50,7 @@ return = greturn . Identity
 
 (>>=)
   :: GradedMonad m i t
-  => AppendMany xs
+  => AppendTensored xs
   => m xs a
   -> (a -> m ys b)
   -> m (xs ++ ys) b
@@ -120,7 +58,7 @@ return = greturn . Identity
 
 (>>)
   :: GradedMonad m i t
-  => AppendMany xs
+  => AppendTensored xs
   => m xs a
   -> m ys b
   -> m (xs ++ ys) b
--- src/Control/Monad/Graded/Writer.hs
+++ src/Control/Monad/Graded/Writer.hs
@@ -11,28 +11,29 @@ module Control.Monad.Graded.Writer where
 
 import Data.Functor.Identity
 import Data.Functor.Compose
+import Control.Category.Tensor.Expr
 import Control.Monad.Graded hiding ((>>=), return)
 import Control.Monad.Graded.Writer.Class
 import Control.Monad.Writer
 
-newtype WriterT' m w a = WriterT' { runWriterT' :: (m (a, Many (,) () w)) }
-  deriving (Functor) via  (WriterT (Many (,) () w) m)
+newtype WriterT' m w a = WriterT' { runWriterT' :: (m (a, Tensored (,) () w)) }
+  deriving (Functor) via  (WriterT (Tensored (,) () w) m)
 
 instance Monad m => GradedMonad (WriterT' m) () (,)
   where
   greturn:: Identity ~> WriterT' m '[]
-  greturn (Identity x) = WriterT' $ pure (x, ANil ())
+  greturn (Identity x) = WriterT' $ pure (x, Tensored ())
 
   gjoin
-    :: AppendMany xs
+    :: AppendTensored xs
     => Compose (WriterT' m xs) (WriterT' m ys)
     ~> WriterT' m (xs ++ ys)
   gjoin (Compose (WriterT' mma)) = WriterT' $ do
     (WriterT' ma, logs) <- mma
     (a, logs') <- ma
-    pure $ (a, appendMany (logs, logs'))
+    pure $ (a, appendTensored (logs, logs'))
 
 instance Monad m => GradedMonadWriter (WriterT' m)
   where
   gtell :: w -> WriterT' m '[w] ()
-  gtell w = WriterT' $ pure ((), ACons (w, ANil ()))
+  gtell w = WriterT' $ pure ((), Tensored (w, ()) )
